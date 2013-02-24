13c8408a1cbeff72119c8c347a61efa4556bd735
Seth Malaki <seth.malaki@gmail.com>
2013-02-24 09:58:19 +0800

    initial commit

---
+++ meteor-call-error.js
@@ -0,0 +1,69 @@
+if (Meteor.isClient) {
+
+  Template.testbutton.events({
+    'click input' : function () {
+      if (typeof console !== 'undefined') {
+	Meteor.call('callme', function(err, res) {
+	    if(!err) {
+		alert("success!");
+	    } else {
+		Session.set("error3", err.reason);
+	    }
+        });
+      }
+    }
+  });
+
+  Template.error1.message = function() {
+    try {
+       return Session.get("error1");
+    } catch(err) {}
+  }
+  Template.error2.message = function() {
+    try {
+       return Session.get("error2");
+    } catch(err) {}
+  }
+  Template.error3.message = function() {
+    try {
+       return Session.get("error3");
+    } catch(err) {}
+  }
+
+  Meteor.startup(function() {
+	// 1 : directly on startup 
+	Meteor.call('callme', function(err, res) {
+	    if(!err) {
+		alert("success!");
+	    } else {
+		Session.set("error1", err.reason);
+	    }
+        });	
+
+	Meteor.autorun(function() {
+	    // 2 : inside autorun
+   	    Meteor.call('callme', function(err, res) {
+	        if(!err) {
+		    alert("success!");
+	        } else {
+		    Session.set("error2", err.reason);
+                }
+            });
+	});
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.methods({
+	'callme' : function () {
+	    throw new Meteor.Error(403, "Error received");
+            return false;
+        }
+  });
+
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
+
+
